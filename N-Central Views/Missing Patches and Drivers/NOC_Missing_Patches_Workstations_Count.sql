958f6b472f9d2633f79d576c3a9867ef0fbc33d9
Tom-NOCAutomationLead <113442900+Tom-NOCAutomationLead@users.noreply.github.com>
2025-05-22 17:37:46 +0100

    Updated to parent customer
    Parent customer instead of site

--- N-Central Views/Missing Patches and Drivers/NOC_Missing_Patches_Workstations_Count.sql
+++ N-Central Views/Missing Patches and Drivers/NOC_Missing_Patches_Workstations_Count.sql
@@ -2,10 +2,11 @@ SELECT   dbo.device.deviceid AS id, dbo.device.longname, dbo.device.deviceclass,
                          COUNT(CASE dbo.NOC_Missing_Patches_Workstations_tbl.Severity WHEN 'Critical' THEN 1 ELSE NULL END) AS MissingCritical, 
                          COUNT(CASE dbo.NOC_Missing_Patches_Workstations_tbl.Severity WHEN 'Important' THEN 1 ELSE NULL END) AS MissingImportant, 
                          COUNT(CASE dbo.NOC_Missing_Patches_Workstations_tbl.Severity WHEN 'Moderate' THEN 1 ELSE NULL END) AS MissingModerate, 
-                         COUNT(CASE dbo.NOC_Missing_Patches_Workstations_tbl.Severity WHEN 'Unspecified' THEN 1 ELSE NULL END) AS MissingUnspecified, dbo.customer.customerid, 
-                         dbo.customer.customername, dbo.device.deleted
+                         COUNT(CASE dbo.NOC_Missing_Patches_Workstations_tbl.Severity WHEN 'Unspecified' THEN 1 ELSE NULL END) AS MissingUnspecified, dbo.device.deleted, 
+                         customer_parent.customerid, customer_parent.customername
 FROM         dbo.NOC_Missing_Patches_Workstations_tbl RIGHT OUTER JOIN
                          dbo.device ON dbo.NOC_Missing_Patches_Workstations_tbl.deviceid = dbo.device.deviceid INNER JOIN
-                         dbo.customer ON dbo.device.customerid = dbo.customer.customerid
+                         dbo.customer ON dbo.device.customerid = dbo.customer.customerid INNER JOIN
+                         dbo.customer AS customer_parent ON dbo.customer.parentid = customer_parent.customerid
 WHERE     (dbo.device.deviceclass NOT LIKE '%Server%') AND (dbo.device.deleted <> 'True') AND (dbo.device.patchenabled = 1)
-GROUP BY dbo.device.deviceid, dbo.device.longname, dbo.device.deviceclass, dbo.customer.customerid, dbo.customer.customername, dbo.device.deleted
+GROUP BY dbo.device.deviceid, dbo.device.longname, dbo.device.deviceclass, dbo.device.deleted, customer_parent.customerid, customer_parent.customername

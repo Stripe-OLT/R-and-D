edd7a90be8a5bc3471dc974882a0960ccd0f8738
Tom-NOCAutomationLead <113442900+Tom-NOCAutomationLead@users.noreply.github.com>
2025-11-18 11:45:54 +0000

    Update NOC_Missing_Patches_Wks_Count_Top_2.sql

--- N-Central Views/Missing Patches and Drivers/NOC_Missing_Patches_Wks_Count_Top_2.sql
+++ N-Central Views/Missing Patches and Drivers/NOC_Missing_Patches_Wks_Count_Top_2.sql
@@ -1,3 +1,3 @@
-SELECT        dbo.NOC_Missing_Patches_Workstations_Count_Top.id, dbo.NOC_LatestUser_2.username, dbo.NOC_LatestUser_2.userdomain
+SELECT        dbo.NOC_LatestUser_2.username, dbo.NOC_LatestUser_2.userdomain, dbo.NOC_Missing_Patches_Workstations_Count_Top.*
 FROM            dbo.NOC_Missing_Patches_Workstations_Count_Top LEFT OUTER JOIN
                          dbo.NOC_LatestUser_2 ON dbo.NOC_Missing_Patches_Workstations_Count_Top.id = dbo.NOC_LatestUser_2.deviceid
